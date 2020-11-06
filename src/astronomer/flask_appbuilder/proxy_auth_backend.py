ab81973ca8d42e78b84cec80126dbb48c5fdce01
James Timmins <james@astronomer.io>
2020-11-06 12:49:34 -0800

    Add proxy backend.

---
+++ src/astronomer/flask_appbuilder/proxy_auth_backend.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Astronomer Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Basic authentication backend"""
+from functools import wraps
+from typing import Callable, Optional, Tuple, TypeVar, Union, cast
+
+from flask import Response
+from flask_login import current_user, login_user
+from requests.auth import AuthBase
+
+CLIENT_AUTH: Optional[Union[Tuple[str, str], AuthBase]] = None
+
+
+def init_app(_):
+    """Initializes authentication backend"""
+
+
+T = TypeVar("T", bound=Callable)  # pylint: disable=invalid-name
+
+
+def requires_authentication(function: T):
+    """Decorator for functions that require authentication"""
+
+    if not current_user.is_anonymous:
+        return current_user
+
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        if not current_user.is_anonymous:
+            login_user(current_user, remember=False)
+            return function(*args, **kwargs)
+
+        return Response("Unauthorized", 401, {"WWW-Authenticate": "Basic"})
+
+    return cast(T, decorated)
